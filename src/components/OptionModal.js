93a50e73f0051420be9d376ff152c267de4d8feb
Amir Kaudinov <kaudinovamir@gmail.com>
2019-12-19 18:17:56 -0700

    - Adding styling to the modal - Adding mobile responsive styling - Adding in a favicon

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -4,12 +4,14 @@ import Modal from 'react-modal'
 
 const OptionModal = ({selectedOption, onCloseModal}) => ( //short hand/implicit return without {return(jsx here);}
     <Modal
+        className="modal" //custom classname for the modal content container
         isOpen={!!selectedOption}
         contentLabel="Selected Option"
+        closeTimeoutMS={200} //wait a certain time(200 miliseconds) before gutting the div, removing it from the DOM
         onRequestClose={onCloseModal}>
-            <h3>Selected Option</h3>
-            {selectedOption && <p>{selectedOption}</p>}
-            <button onClick={onCloseModal}>Okay</button>
+            <h3 className="modal__title">Selected Option</h3>
+            {selectedOption && <p className="modal__body">{selectedOption}</p>}
+            <button className="button" onClick={onCloseModal}>Okay</button>
     </Modal>
 );
 
